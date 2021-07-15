530184db7f96f8a0b2938b8e5372e6e6b5345116
arturoeanton <arturoeanton@gmail.com>
2021-07-15 01:02:07 -0300

    init

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/arturoeanton/go-notebook
 go 1.16
 
 require (
-	github.com/arturoeanton/go-echo-live-view v0.0.0-20210714165620-19a2d74d4a3a
+	github.com/arturoeanton/go-echo-live-view v0.0.0-20210714232313-ae486449a5ab
+	github.com/arturoeanton/gocommons v0.0.0-20210613045617-59f224587f91
 	github.com/cosmos72/gomacro v0.0.0-20210624153544-b4935e406a41
 	github.com/gomarkdown/markdown v0.0.0-20210514010506-3b9f47219fe7
 	github.com/google/uuid v1.2.0
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -9,6 +11,7 @@ import (
 
 	"github.com/arturoeanton/go-echo-live-view/components"
 	"github.com/arturoeanton/go-echo-live-view/liveview"
+	"github.com/arturoeanton/gocommons/utils"
 	"github.com/cosmos72/gomacro/fast"
 	"github.com/gomarkdown/markdown"
 	"github.com/google/uuid"
@@ -16,98 +19,169 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+var (
+	note         Note = Note{Order: make([]string, 0), Items: make(map[string]Item)}
+	notebookFile      = "data.gonote.json"
+)
+
+func MapToJSONFile(file string, data interface{}) {
+	jsonString, err := json.Marshal(data)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	utils.StringToFile(file, string(jsonString))
+}
+
+type Note struct {
+	Order []string `json:"order"`
+	Items map[string]Item
+}
+
+type Item struct {
+	MD    *TextMD `json:"md,omitempty"`
+	Shell *Shell  `json:"shell,omitempty"`
+}
+
+type LinkMenu struct {
+	Driver  *liveview.ComponentDriver `json:"-"`
+	Caption string                    `json:"caption"`
+	Id      string                    `json:"id"`
+}
+
+func (t *LinkMenu) Start() {
+	t.Driver.Commit()
+}
+
+func (t *LinkMenu) GetTemplate() string {
+	return `<a  href="#" onClick="send_event('{{.Id}}','Click')" >{{.Caption}}</a>`
+}
+
 type TextMD struct {
-	Driver *liveview.ComponentDriver
-	UUID   string
-	Id     string
-	CodeMD string
-	Result string
+	Driver      *liveview.ComponentDriver `json:"-"`
+	UUID        string                    `json:"uuid"`
+	Id          string                    `json:"id"`
+	CodeMD      string                    `json:"code_md"`
+	Result      string                    `json:"result"`
+	CaptionPlay string                    `json:"caption_play"`
 }
 
 func (t *TextMD) GetTemplate() string {
-	return `<hr/>
-	<fieldset>
-	<legend>Text</legend>
-	{{if eq .Result "" }} 
-	<textarea  id="{{.UUID}}_text_md" style="width: 100%;" >{{.CodeMD}}</textarea>
-	{{end }} 
-	{{if ne .Result "" }} 
-	<fieldset>
-	{{.Result}}
-	</fieldset>
-	{{end}}
-	<hr/>
-	<button id="{{.UUID}}_play" onclick="send_event('{{.Id}}', 'Click')" >Convert</button>
-	</fieldset>
-
+	return `
+	<br>
+	<div id="{{.UUID}}_div_md" style="background: white;">
+		<fieldset style="border-radius: 5px">
+			{{if eq .Result "" }} 
+			<textarea  oninput="autosize(this)" onfocus="autosize(this)" id="{{.UUID}}_text_md" style="width: 100%; height: 50px; padding: 5px; background: cornsilk;" >{{.CodeMD}}</textarea>
+			{{end }} 
+			{{if ne .Result "" }} 
+			<fieldset style="border-width: 0px;">
+			{{.Result}}
+			</fieldset>
+			{{end}}
+			<hr/>
+			<button id="{{.UUID}}_play" onclick="send_event('{{.Id}}', 'Click')" >{{.CaptionPlay}}</button>
+			<button id="{{.UUID}}_remove" onclick="send_event('{{.Id}}', 'Remove', '{{.UUID}}')" >Remove</button>
+		</fieldset>
+	</div>
 	`
 }
 
 func (t *TextMD) Start() {
 	t.UUID = uuid.New().String()
 	t.Driver.Commit()
+	if !utils.ContainsString(note.Order, t.Id) {
+		note.Order = append(note.Order, t.Id)
+	}
+
+	note.Items[t.Id] = Item{MD: t}
+	MapToJSONFile(notebookFile, note)
 }
 
 func (t *TextMD) Click(data interface{}) {
 	if t.Result != "" {
 		t.Result = ""
+		t.CaptionPlay = "Print"
 		t.Driver.Commit()
+		note.Items[t.Id] = Item{MD: t}
+		MapToJSONFile(notebookFile, note)
 		return
 	}
+	t.CaptionPlay = "Edit"
 	t.CodeMD = t.Driver.GetValue(t.UUID + "_text_md")
 	md := []byte(t.CodeMD)
 	t.Result = string(markdown.ToHTML(md, nil, nil))
 	t.Driver.Commit()
+	note.Items[t.Id] = Item{MD: t}
+	MapToJSONFile(notebookFile, note)
+}
+
+func (t *TextMD) Remove(data interface{}) {
+	delete(note.Items, t.Id)
+	t.Driver.Remove(t.UUID + "_div_md")
+	MapToJSONFile(notebookFile, note)
 }
 
 type Shell struct {
-	Driver      *liveview.ComponentDriver
-	Interpreter *fast.Interp
-	Debug       bool
-	UUID        string
-	Id          string
-	Code        string
-	CaptionPlay string
-	Result      string
-	Out         string
-	Err         string
+	Driver      *liveview.ComponentDriver `json:"-"`
+	Interpreter *fast.Interp              `json:"-"`
+	Debug       bool                      `json:"debug"`
+	UUID        string                    `json:"uuid"`
+	Id          string                    `json:"id"`
+	Code        string                    `json:"code"`
+	CaptionPlay string                    `json:"caption_play"`
+	Result      string                    `json:"-"`
+	Out         string                    `json:"-"`
+	Err         string                    `json:"-"`
 }
 
 func (t *Shell) Start() {
-	t.CaptionPlay = "Play"
 	t.Out = ""
 	t.UUID = uuid.New().String()
 	t.Driver.Commit()
+	if !utils.ContainsString(note.Order, t.Id) {
+		note.Order = append(note.Order, t.Id)
+	}
+	note.Items[t.Id] = Item{Shell: t}
+	MapToJSONFile(notebookFile, note)
 }
 
 func (t *Shell) GetTemplate() string {
-	return `<hr/>
-	<fieldset>
-	<legend>Code</legend>
-	<textarea  id="{{.UUID}}_code" style="width: 100%;" >{{.Code}}</textarea>
-	{{if ne .Result "" }} 
-	<fieldset>
-	<legend>Result</legend>
-	{{.Result}}
-	</fieldset>
-	{{end}}
-	{{if ne .Out "" }} 
-	<fieldset>
-	<legend>Output</legend>
-	{{.Out}} 
-	</fieldset>
-	{{end}}
-	{{if ne .Err "" }} 
-	<fieldset>
-	<legend>Stderr</legend>
-	{{.Err}} 
-	</fieldset>
-	{{end}}
-	<hr/>
-	<button id="{{.UUID}}_play" onclick="send_event('{{.Id}}', 'Click')" >{{.CaptionPlay}}</button>
-	</fieldset>`
+	return `
+	<br>
+	<div id="{{.UUID}}_div_code" style="background: white;">
+		<fieldset style="border-radius: 5px">
+			<textarea  oninput="autosize(this)" onfocus="autosize(this)" id="{{.UUID}}_code" style="width: 100%; height: 50px; padding: 5px; background: darkseagreen;" >{{.Code}}</textarea>
+			{{if ne .Result "" }} 
+			<fieldset style="background: aliceblue;">
+				<legend>Result</legend>
+				{{.Result}}
+			</fieldset>
+			{{end}}
+			{{if ne .Out "" }} 
+			<fieldset style="background: aliceblue;">
+				<legend>Output</legend>
+				{{.Out}} 
+			</fieldset>
+			{{end}}
+			{{if ne .Err "" }} 
+			<fieldset>
+				<legend>Stderr</legend>
+				{{.Err}} 
+			</fieldset>
+			{{end}}
+			<hr/>
+			<button id="{{.UUID}}_play" onclick="send_event('{{.Id}}', 'Click')" >{{.CaptionPlay}}</button>
+			<button id="{{.UUID}}_remove" onclick="send_event('{{.Id}}', 'Remove', '{{.UUID}}')" >Remove</button>
+		</fieldset>
+	</div>`
 }
 
+func (t *Shell) Remove(data interface{}) {
+	delete(note.Items, t.Id)
+	t.Driver.Remove(t.UUID + "_div_code")
+	MapToJSONFile(notebookFile, note)
+}
 func (t *Shell) Click(data interface{}) {
 	old := os.Stdout // keep backup of the real stdout
 	oldErr := os.Stderr
@@ -138,6 +212,8 @@ func (t *Shell) Click(data interface{}) {
 		t.Out = <-outC
 
 		t.Driver.Commit()
+		note.Items[t.Id] = Item{Shell: t}
+		MapToJSONFile(notebookFile, note)
 	}()
 
 	values, types := t.Interpreter.Eval(t.Code)
@@ -153,45 +229,117 @@ func (t *Shell) Click(data interface{}) {
 }
 
 func main() {
+
+	filename := flag.String("f", "default.gonote.json", "GoNotebookFile")
+	port := flag.String("p", ":1323", "port")
+	flag.Parse()
+	notebookFile = *filename
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	home := liveview.PageControl{
-		Title:  "Go-Notebook",
-		Lang:   "en",
+		Title: "Go-Notebook",
+		Lang:  "en",
+		HeadCode: `
+		<script src="https://rawgit.com/jackmoore/autosize/master/dist/autosize.min.js"></script>
+		`,
+		Css: `
+		body {margin:0;}
+
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  top: 0;
+  width: 100%;
+
+}
+
+.navbar a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.navbar a:hover {
+  background: #ddd;
+  color: black;
+}
+
+#main_div {
+	padding: 16px;
+	margin-top: 30px;
+  }
+		`,
 		Path:   "/",
 		Router: e,
 	}
 
 	home.Register(func() *liveview.ComponentDriver {
 		interp := fast.New()
-		page := components.NewLayout("notebook", `{{ mount "button_add_code"}} {{mount "button_add_md"}}`)
-		button_add_code := liveview.NewDriver("button_add_code", &components.Button{Caption: "Add Code"})
-		button_add_code.Events["Click"] = func(data interface{}) {
-			button := button_add_code.Component.(*components.Button)
-			button.I++
-			content := button_add_code.GetHTML("content")
-			content += `<div id="shell` + fmt.Sprint(button.I) + `"><div>`
-			button_add_code.SetHTML("content", content)
-			idShell := "shell" + fmt.Sprint(button.I)
-			shell := liveview.NewDriver(idShell, &Shell{Debug: true, Interpreter: interp})
-			page.MountWithStart(idShell, shell)
-		}
-		button_add_md := liveview.NewDriver("button_add_md", &components.Button{Caption: "Add Text"})
-		button_add_md.Events["Click"] = func(data interface{}) {
-			button := button_add_md.Component.(*components.Button)
-			button.I++
-			content := button_add_md.GetHTML("content")
-			content += `<div id="text` + fmt.Sprint(button.I) + `"><div>`
-			button_add_md.SetHTML("content", content)
-			idText := "text" + fmt.Sprint(button.I)
-			textMD := liveview.NewDriver(idText, &TextMD{})
-			page.MountWithStart(idText, textMD)
-		}
-		return page.Mount(button_add_code).Mount(button_add_md)
+		page := components.NewLayout("notebook", `
+		<div class="navbar">
+		{{ mount "link_add_code"}} 
+		{{mount "link_add_md"}}
+		{{mount "link_reload"}}
+		</div>  
+		<div id="main_div">
+		</div>
+		`)
 
+		link_add_code := liveview.NewDriver("link_add_code", &LinkMenu{Caption: "+ Code"})
+		link_add_code.Events["Click"] = func(data interface{}) {
+			idShell := "shell_" + uuid.NewString()
+			newShell := &Shell{Debug: true, Interpreter: interp, CaptionPlay: "Play"}
+			addShell(idShell, page, link_add_code, newShell)
+		}
+		link_add_md := liveview.NewDriver("link_add_md", &LinkMenu{Caption: "+ Text"})
+		link_add_md.Events["Click"] = func(data interface{}) {
+			idText := "text_" + uuid.NewString()
+			addMD(idText, page, link_add_md, &TextMD{CaptionPlay: "Print"})
+		}
+
+		link_reload := liveview.NewDriver("link_reload", &LinkMenu{Caption: "Reload"})
+		link_reload.Events["Click"] = func(data interface{}) {
+			jsonString, _ := utils.FileToString(notebookFile)
+			json.Unmarshal([]byte(jsonString), &note)
+			for _, key := range note.Order {
+				item := note.Items[key]
+				if item.MD != nil {
+					addMD(key, page, link_reload, item.MD)
+				}
+				if item.Shell != nil {
+					item.Shell.Interpreter = interp
+					item.Shell.CaptionPlay = "Play"
+					addShell(key, page, link_reload, item.Shell)
+				}
+			}
+		}
+
+		return page.Mount(link_add_code).Mount(link_add_md).Mount(link_reload)
 	})
 
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(*port))
+}
+
+func addShell(idShell string, page, driver *liveview.ComponentDriver, newShell *Shell) {
+	main_div := driver.GetHTML("main_div")
+	main_div += `<div id="` + idShell + `"><div>`
+	driver.SetHTML("main_div", main_div)
+	shell := liveview.NewDriver(idShell, newShell)
+	page.MountWithStart(idShell, shell)
+}
+
+func addMD(idText string, page, driver *liveview.ComponentDriver, newTextMD *TextMD) {
+	main_div := driver.GetHTML("main_div")
+	main_div += `<div id="` + idText + `"><div>`
+	driver.SetHTML("main_div", main_div)
+	textMD := liveview.NewDriver(idText, newTextMD)
+	page.MountWithStart(idText, textMD)
 }
